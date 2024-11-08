e7d9a7f0e16b9bbcc21ee8e749b9d77c6cd8ad80
Memo <969051037@qq.com>
2024-11-08 11:11:06 +0800

    上传回调类型

--- WebGLTools/TelegramSDK.js
+++ WebGLTools/TelegramSDK.js
@@ -3,6 +3,6 @@ window.TelegramSDK = {
         var { retrieveLaunchParams } = window.telegramApps.sdk;
         var launchParams = retrieveLaunchParams();
         var backData = {MethodName: "Init", Code: "1", Data: JSON.stringify(launchParams)};
-        unityInstanceRef.SendMessage("TelegramSDKManger", "TelegramCallBack", JSON.stringify(backData));
+        unityInstanceRef.SendMessage("SDKCallbackMono", "TelegramCallBack", JSON.stringify(backData));
     },
 }
