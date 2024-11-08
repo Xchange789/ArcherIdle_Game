36f923db719b326b31d873e4416d501b121fe43e
Memo <969051037@qq.com>
2024-11-08 14:24:46 +0800

    修改分享逻辑

--- WebGLTools/TelegramSDK.js
+++ WebGLTools/TelegramSDK.js
@@ -5,4 +5,27 @@ window.TelegramSDK = {
         var backData = {TypeName:"TelegranSDKCallback",MethodName: "Init", Code: "1", Data: JSON.stringify(launchParams)};
         unityInstanceRef.SendMessage("SDKCallbackMono", "CallbackToUnity", JSON.stringify(backData));
     },
+
+    DoShare : function (url, text)
+    {
+        var _url = UTF8ToString(url);
+        var _text = UTF8ToString(text);
+
+        var { shareURL } = window.telegramApps.sdk;
+        shareURL(_url, _text);
+
+        var backData = {TypeName:"TelegranSDKCallback",MethodName: "DoShareURL", Code: "0", Data: "OK"};
+        unityInstanceRef.SendMessage("SDKCallbackMono", "CallbackToUnity", JSON.stringify(backData));
+    },
 }
+
+(function (){
+    // receive event
+    window.TelegramGameProxy.receiveEvent =
+        window.Telegram.WebView.receiveEvent  =
+            window.TelegramGameProxy_receiveEvent =
+                (eventType, eventData) => {         
+                    eventData = { eventType, eventData };
+                    unityInstanceRef.SenMessage("TelegramSDKManger", "TelegramEvents", JSON.stringify(eventData));
+                };
+})();
