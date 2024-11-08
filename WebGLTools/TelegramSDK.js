4c320dab0f792df52a6cb18c9a03866fc60acb04
Memo <969051037@qq.com>
2024-11-08 14:54:08 +0800

    修改分享

--- WebGLTools/TelegramSDK.js
+++ WebGLTools/TelegramSDK.js
@@ -8,11 +8,11 @@ window.TelegramSDK = {
 
     DoShare : function (url, text)
     {
-        var _url = UTF8ToString(url);
-        var _text = UTF8ToString(text);
+        // var _url = UTF8ToString(url);
+        // var _text = UTF8ToString(text);
 
         var { shareURL } = window.telegramApps.sdk;
-        shareURL(_url, _text);
+        shareURL(url, text);
 
         var backData = {TypeName:"TelegranSDKCallback",MethodName: "DoShareURL", Code: "0", Data: "OK"};
         unityInstanceRef.SendMessage("SDKCallbackMono", "CallbackToUnity", JSON.stringify(backData));
