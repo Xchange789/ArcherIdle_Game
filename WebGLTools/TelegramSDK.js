a9b02949d50ee6b60425f2a7307f79ad26b0e297
Memo <969051037@qq.com>
2024-11-20 16:13:25 +0800

    no message

--- WebGLTools/TelegramSDK.js
+++ WebGLTools/TelegramSDK.js
@@ -4,6 +4,15 @@ window.TelegramSDK = {
         var launchParams = retrieveLaunchParams();
         var backData = {TypeName:"TelegranSDKCallback",MethodName: "Init", Code: "1", Data: JSON.stringify(launchParams)};
         unityInstanceRef.SendMessage("SDKCallbackMono", "CallbackToUnity", JSON.stringify(backData));
+        
+            // receive event
+    window.TelegramGameProxy.receiveEvent =
+    window.Telegram.WebView.receiveEvent  =
+        window.TelegramGameProxy_receiveEvent =
+            (eventType, eventData) => {         
+                eventData = { eventType, eventData };
+                unityInstanceRef.SenMessage("SDKCallbackMono", "TelegramEvents", JSON.stringify(eventData));
+            };
     },
 
     DoShare : function (url, text)
@@ -47,12 +56,5 @@ window.TelegramSDK = {
 }
 
 // (function (){
-//     // receive event
-//     window.TelegramGameProxy.receiveEvent =
-//         window.Telegram.WebView.receiveEvent  =
-//             window.TelegramGameProxy_receiveEvent =
-//                 (eventType, eventData) => {         
-//                     eventData = { eventType, eventData };
-//                     unityInstanceRef.SenMessage("SDKCallbackMono", "TelegramEvents", JSON.stringify(eventData));
-//                 };
+
 // })();
