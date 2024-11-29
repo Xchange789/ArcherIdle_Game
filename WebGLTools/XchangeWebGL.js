f79a61bef72b3855bff6b754050d14bd9055d734
Sans <Sans@mail.com>
2024-11-29 17:23:37 +0800

    111
    111

--- WebGLTools/XchangeWebGL.js
+++ WebGLTools/XchangeWebGL.js
@@ -1,5 +1,6 @@
 window.XchangeWebGL = {
     CopyToClipboard: function (text) {
+        /*
         navigator.clipboard.writeText(text).then(function () {
             var backData = {MethodName: "CopyToClipboard", Code: "1", Data: "Seccess"};
             unityInstanceRef.SendMessage('XchangeWebGL', 'XchangeWebGLCallBack', JSON.stringify(backData));
@@ -8,6 +9,29 @@ window.XchangeWebGL = {
             var backData = {MethodName: "CopyToClipboard", Code: "0", Data: error};
             unityInstanceRef.SendMessage('XchangeWebGL', 'XchangeWebGLCallBack', JSON.stringify(backData));
         });
+        */
+        // 创建一个临时元素
+        var tempElement = document.createElement('textarea');
+        // 将文本值复制到临时元素
+        tempElement.value = text;
+        // 将临时元素添加到文档中
+        document.body.appendChild(tempElement);
+        // 选择临时元素中的文本
+        tempElement.select();
+        // 执行复制操作
+        var code = "1";
+        try {
+            document.execCommand('copy');
+        } catch (err) {
+            console.log('Unable to copy text to clipboard');
+            code = "0";
+        }
+        // 移除临时元素
+        document.body.removeChild(tempElement);
+        
+        var backData = {MethodName: "CopyToClipboard", Code: code, Data: "Seccess"};
+        unityInstanceRef.SendMessage('XchangeWebGL', 'XchangeWebGLCallBack', JSON.stringify(backData));
+        
     },
 
 
