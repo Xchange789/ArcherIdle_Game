bb2675517d9d25fc33a272db7df2b73b489aa8a2
Sans <Sans@mail.com>
2024-12-03 11:55:34 +0800

    Update Platform.js

--- WebGLTools/Platform.js
+++ WebGLTools/Platform.js
@@ -85,7 +85,7 @@
             });
         }
     }
-    const platformA = new Platform(); 
+    const TelegramPlatformA = new Platform(); 
 }());
 
-var platformB = new Platform();
+var TelegramPlatformB = new Platform();
