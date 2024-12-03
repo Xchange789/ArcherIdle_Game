12cf87ef451bdf72ea3f9710b5f9605d9357db70
Sans <Sans@mail.com>
2024-12-03 12:50:13 +0800

    1
    1

--- WebGLTools/Platform.js
+++ WebGLTools/Platform.js
@@ -1,92 +1,89 @@
 ﻿class Platform {
-	reload() {
-		window.location.reload();
-	}
-	init() {
-		this.m_telegram = window['Telegram'];
-		this.m_telegram.WebApp.enableClosingConfirmation();
-	}
-	openLink(url) {
-		this.m_telegram.WebApp.openLink(url);
-	}
-	openTelegramLink(url) {
-		this.m_telegram.WebApp.openTelegramLink(url);
-	}
-	openInvoice(url) {
-		return new Promise((resolve, reject) => {
-			this.m_telegram.WebApp.openInvoice(url, (status) => {
-				console.log("payment status==>" + status);
-				if (status == "paid") {
-					resolve();
-				}
-			});
-		});
-	}
-	enableClosingConfirmation() {
-		this.m_telegram.WebApp.enableClosingConfirmation();
-	}
-	disableClosingConfirmation() {
-		this.m_telegram.WebApp.disableClosingConfirmation();
-	}
-	closeApp() {
-		this.m_telegram.WebApp.close();
-	}
-	clipboard(text) {
-		return new Promise((resolve, reject) => {
-			if (!text) {
-				reject();
-				return;
-			}
-			this.writeText(text).then(() => {
-				resolve();
-			}).catch(() => {
-				this.execCopy(text).then(() => {
-					resolve();
-				}).catch(() => {
-					reject();
-				});
-			});
-		});
-	}
-	writeText(text) {
-		return new Promise((resolve, reject) => {
-			if (!navigator || !navigator.clipboard) {
-				reject();
-				return;
-			}
-			navigator.clipboard.writeText(text).then(() => {
-				resolve();
-			}).catch(() => {
-				console.log('writeText error');
-				reject();
-			});
-		});
-	}
-	execCopy(text) {
-		return new Promise((resolve, reject) => {
-			let textArea = document.createElement("textarea");
-			textArea.value = text;
-			textArea.style.position = "absolute";
-			textArea.style.opacity = "0";
-			textArea.style.left = "-999999px";
-			textArea.style.top = "-999999px";
-			document.body.appendChild(textArea);
-			textArea.focus();
-			textArea.select();
-			let success = document.execCommand('copy');
-			textArea.remove();
-			if (success) {
-				resolve();
-			}
-			else {
-				reject();
-			}
-		});
-	}
+    reload() {
+        window.location.reload();
+    }
+    init() {
+        this.m_telegram = window['Telegram'];
+        this.m_telegram.WebApp.enableClosingConfirmation();
+    }
+    openLink(url) {
+        this.m_telegram.WebApp.openLink(url);
+    }
+    openTelegramLink(url) {
+        this.m_telegram.WebApp.openTelegramLink(url);
+    }
+    openInvoice(url) {
+        return new Promise((resolve, reject) => {
+            this.m_telegram.WebApp.openInvoice(url, (status) => {
+                console.log("payment status==>" + status);
+                if (status == "paid") {
+                    resolve();
+                }
+            });
+        });
+    }
+    enableClosingConfirmation() {
+        this.m_telegram.WebApp.enableClosingConfirmation();
+    }
+    disableClosingConfirmation() {
+        this.m_telegram.WebApp.disableClosingConfirmation();
+    }
+    closeApp() {
+        this.m_telegram.WebApp.close();
+    }
+    clipboard(text) {
+        return new Promise((resolve, reject) => {
+            if (!text) {
+                reject();
+                return;
+            }
+            this.writeText(text).then(() => {
+                resolve();
+            }).catch(() => {
+                this.execCopy(text).then(() => {
+                    resolve();
+                }).catch(() => {
+                    reject();
+                });
+            });
+        });
+    }
+    writeText(text) {
+        return new Promise((resolve, reject) => {
+            if (!navigator || !navigator.clipboard) {
+                reject();
+                return;
+            }
+            navigator.clipboard.writeText(text).then(() => {
+                resolve();
+            }).catch(() => {
+                console.log('writeText error');
+                reject();
+            });
+        });
+    }
+    execCopy(text) {
+        return new Promise((resolve, reject) => {
+            let textArea = document.createElement("textarea");
+            textArea.value = text;
+            textArea.style.position = "absolute";
+            textArea.style.opacity = "0";
+            textArea.style.left = "-999999px";
+            textArea.style.top = "-999999px";
+            document.body.appendChild(textArea);
+            textArea.focus();
+            textArea.select();
+            let success = document.execCommand('copy');
+            textArea.remove();
+            if (success) {
+                resolve();
+            }
+            else {
+                reject();
+            }
+        });
+    }
 }
 
-(function (){
-	
-}());
 
 var platform = new Platform();
--- WebGLTools/XchangeWebGL.js
+++ WebGLTools/XchangeWebGL.js
@@ -1,79 +1,12 @@
-window.XchangeWebGL = {
-
-    resolve: function (){
-        var backData = {MethodName: "CopyToClipboard", Code: "1", Data: "Seccess"};
-        unityInstanceRef.SendMessage('XchangeWebGL', 'XchangeWebGLCallBack', JSON.stringify(backData));
-    },
-
-    reject: function (error){
-        var backData = {MethodName: "CopyToClipboard", Code: "0", Data: error};
-        unityInstanceRef.SendMessage('XchangeWebGL', 'XchangeWebGLCallBack', JSON.stringify(backData));
-    },
-
-    execCopy: function (text) {
-        return new Promise((resolve, reject) => {
-            let textArea = document.createElement("textarea");
-            textArea.value = text;
-            textArea.style.position = "absolute";
-            textArea.style.opacity = "0";
-            textArea.style.left = "-999999px";
-            textArea.style.top = "-999999px";
-            document.body.appendChild(textArea);
-            textArea.focus();
-            textArea.select();
-            let success = document.execCommand('copy');
-            textArea.remove();
-            if (success) {
-                alert(success);
-                resolve();
-            }
-            else {
-                alert(success);
-                reject();
-            }
-        });
-    },
-
-    CopyToClipboard: function (text) {
-
         platform.clipboard(text).then(function()  {
-            alert("success");  
-            XchangeWebGL.resolve(); 
-        }).catch(function (){              
-            alert("fail");             
-            XchangeWebGL.reject(error); 
+            alert("success");
+
+            var backData = {MethodName: "CopyToClipboard", Code: "1", Data: "Seccess"};
+            unityInstanceRef.SendMessage('XchangeWebGL', 'XchangeWebGLCallBack', JSON.stringify(backData));
+            
+        }).catch(function (){
+            alert("fail");
+
+            var backData = {MethodName: "CopyToClipboard", Code: "0", Data: "Fail"};
+            unityInstanceRef.SendMessage('XchangeWebGL', 'XchangeWebGLCallBack', JSON.stringify(backData));
         });
-		
-    },
-
-
-    SendInputToUnity: function () {
-        var inputField = document.getElementById('unityInputField');
-        var inputValue = inputField.value;
-        // 将输入的内容发送到Unity中
-        unityInstanceRef.SendMessage('WebGLTextInput', 'OnInputChanged', inputValue);
-    },
-
-    ShowInputField: function (x, y, width, data) {
-        var inputField = document.getElementById('unityInputField');
-        // inputField.style.left = x + 'px';
-        // inputField.style.top = y + 'px';
-        // inputField.style.width = width + 'px';
-        inputField.value = data;
-        inputField.style.opacity = 1;
-        inputField.focus();
-    },
-
-    HideInputField: function () {
-        var inputField = document.getElementById('unityInputField');
-        unityInstanceRef.SendMessage('WebGLTextInput', 'OnInputChanged', inputField.value);
-        inputField.style.opacity = 0;
-        inputField.blur();
-    },
-
-    SyncDB: function () {
-        FS.syncfs(false, function (err) {
-            if (err) console.log("syncfs error: " + err);
-        });
-    },
-}
