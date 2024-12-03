2914f4f6e5c712f9cf6c27b6f2b51e94a7a5c2c7
Sans <Sans@mail.com>
2024-12-03 12:52:58 +0800

    Update XchangeWebGL.js

--- WebGLTools/XchangeWebGL.js
+++ WebGLTools/XchangeWebGL.js
@@ -1,3 +1,7 @@
+window.XchangeWebGL = {
+    
+    CopyToClipboard: function (text) {
+
         platform.clipboard(text).then(function()  {
             alert("success");
 
@@ -10,3 +14,36 @@
             var backData = {MethodName: "CopyToClipboard", Code: "0", Data: "Fail"};
             unityInstanceRef.SendMessage('XchangeWebGL', 'XchangeWebGLCallBack', JSON.stringify(backData));
         });
+    },
+
+
+    SendInputToUnity: function () {
+        var inputField = document.getElementById('unityInputField');
+        var inputValue = inputField.value;
+        // 将输入的内容发送到Unity中
+        unityInstanceRef.SendMessage('WebGLTextInput', 'OnInputChanged', inputValue);
+    },
+
+    ShowInputField: function (x, y, width, data) {
+        var inputField = document.getElementById('unityInputField');
+        // inputField.style.left = x + 'px';
+        // inputField.style.top = y + 'px';
+        // inputField.style.width = width + 'px';
+        inputField.value = data;
+        inputField.style.opacity = 1;
+        inputField.focus();
+    },
+
+    HideInputField: function () {
+        var inputField = document.getElementById('unityInputField');
+        unityInstanceRef.SendMessage('WebGLTextInput', 'OnInputChanged', inputField.value);
+        inputField.style.opacity = 0;
+        inputField.blur();
+    },
+
+    SyncDB: function () {
+        FS.syncfs(false, function (err) {
+            if (err) console.log("syncfs error: " + err);
+        });
+    },
+}
