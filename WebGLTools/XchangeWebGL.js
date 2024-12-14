49a3db6bbb06321fb244987fc30564c91f2e19d0
Memo <969051037@qq.com>
2024-12-14 17:25:01 +0800

    no message

--- WebGLTools/XchangeWebGL.js
+++ WebGLTools/XchangeWebGL.js
@@ -38,6 +38,7 @@ window.XchangeWebGL = {
         button.style.position = "absolute";
         button.style.left = x + "px";
         button.style.top = y + "px";
+        button.style.opacity =0;
 
 
         var info = JSON.parse(data);
