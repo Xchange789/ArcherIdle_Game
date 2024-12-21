8ac448f98e9296aa51990cac42364a8ab630cf8d
Sans <Sans@mail.com>
2024-12-21 14:22:00 +0800

    1
    1

---
+++ WebGLTools/Platform.js
@@ -0,0 +1,128 @@
+﻿class Platform {
+    reload() {
+        window.location.reload();
+    }
+    init() {
+        this.m_telegram = window['Telegram'];
+        this.m_telegram.WebApp.enableClosingConfirmation();
+    }
+    postEvent(event, jsonData) {
+        this.m_telegram.WebView.postEvent(event, false, jsonData);
+    }
+    openLink(url) {
+        this.m_telegram.WebApp.openLink(url);
+    }
+    openTelegramLink(url) {
+        this.m_telegram.WebApp.openTelegramLink(url);
+    }
+    openInvoice(url) {
+        this.m_telegram.WebApp.openInvoice(url);
+    }
+    enableClosingConfirmation() {
+        this.m_telegram.WebApp.enableClosingConfirmation();
+    }
+    disableClosingConfirmation() {
+        this.m_telegram.WebApp.disableClosingConfirmation();
+    }
+    closeApp() {
+        this.m_telegram.WebApp.close();
+    }
+    clipboard(text) {
+        return new Promise((resolve, reject) => {
+            if (!text) {
+                reject();
+                return;
+            }
+            this.writeText(text).then(() => {
+                resolve();
+            }).catch(() => {
+                this.execCopy(text).then(() => {
+                    resolve();
+                }).catch(() => {
+                    reject();
+                });
+            });
+        });
+    }
+    writeText(text) {
+        return new Promise((resolve, reject) => {
+            if (!navigator || !navigator.clipboard) {
+                reject();
+                return;
+            }
+            navigator.clipboard.writeText(text).then(() => {
+                resolve();
+            }).catch(() => {
+                console.log('writeText error');
+                reject();
+            });
+        });
+    }
+    execCopy(text) {
+        return new Promise((resolve, reject) => {
+            let textArea = document.createElement("textarea");
+            textArea.value = text;
+            textArea.style.position = "absolute";
+            textArea.style.opacity = "0";
+            textArea.style.left = "-999999px";
+            textArea.style.top = "-999999px";
+            document.body.appendChild(textArea);
+            textArea.focus();
+            textArea.select();
+            let success = document.execCommand('copy');
+            textArea.remove();
+            if (success) {
+                resolve();
+            }
+            else {
+                reject();
+            }
+        });
+    }
+    retrieveLaunchParams(){
+        return  {
+            botInline: true,
+            initData:  {
+                auth_date: this.m_telegram.WebApp.initDataUnsafe.auth_date,
+                chat_instance: this.m_telegram.WebApp.initDataUnsafe.chat_instance,
+                chat_type: this.m_telegram.WebApp.initDataUnsafe.chat_type,
+                hash: this.m_telegram.WebApp.initDataUnsafe.hash,
+                user: this.m_telegram.WebApp.initDataUnsafe.user
+            },
+            initDataRaw: this.m_telegram.WebApp.initData,
+            platform: this.m_telegram.WebApp.platform,
+            themeParams: this.m_telegram.WebApp.themeParams,
+            version: this.m_telegram.WebApp.version,
+            startParam: this.m_telegram.WebApp.initDataUnsafe.start_param ?? "" 
+        };
+    }
+    addToHomeScreen()
+    {
+        this.m_telegram.WebApp.addToHomeScreen();
+    }
+    checkHomeScreenStatus(tgEventName)
+    {
+        this.m_telegram.WebApp.checkHomeScreenStatus((status) => {
+            var eventType= tgEventName;
+            var eventJson= JSON.stringify({"status": status});
+            alert(eventJson);
+            var eventData = { eventType, eventJson };
+            unityInstanceRef.SendMessage("SDKCallbackMono", "TelegramEvents", JSON.stringify(eventData));
+        });
+    }
+}
+
+var platform = new Platform();
+
+(function (){
+    platform.init();
+    // Some versions of Telegram don't need the classes above.
+    if (['macos', 'tdesktop', 'weba', 'web', 'webk'].includes(platform.m_telegram.WebApp.platform)) {
+        return;
+    }
+
+    // Expand the application.
+    //postEvent('web_app_expand');
+    //platform.m_telegram.WebApp.expand();
+    platform.m_telegram.WebApp.requestFullscreen();
+})();
---
+++ WebGLTools/XchangeWebGL.js
@@ -0,0 +1,110 @@
+window.XchangeWebGL = {
+    ShowInputField: function (x, y, width, height, data) {
+        var inputField = document.getElementById('unityInputField');
+        inputField.style.width = width + "px";
+        inputField.style.height = height + "px";
+        inputField.style.position = "absolute";
+        inputField.style.left = (x - 4) + "px";
+        inputField.style.top  = (y - 2) + "px";
+        inputField.value = data;
+        inputField.style.visibility = "visible";
+        inputField.style.opacity = 0;
+        inputField.focus();
+    },
+
+    HideInputField: function () {
+        var inputField = document.getElementById('unityInputField');
+        unityInstanceRef.SendMessage('WebGLTextInput', 'OnInputChanged', inputField.value);
+        inputField.style.visibility = "hidden";
+        inputField.blur();
+    },
+
+    SendInputToUnity: function () {
+        var inputField = document.getElementById('unityInputField');
+        var inputValue = inputField.value;
+        // 将输入的内容发送到Unity中
+        unityInstanceRef.SendMessage('WebGLTextInput', 'OnInputChanged', inputValue);
+    },
+
+    SyncDB: function () {
+        FS.syncfs(false, function (err) {
+            if (err) console.log("syncfs error: " + err);
+        });
+    },
+
+    Alert: function (msg) {
+        window.alert(msg);
+    },
+
+    GetWebGLWindowsSize: function (){
+        var backData = {MethodName: "GetWebGLWindowsSize", Code: "1", Data: window.innerWidth + "," + window.innerHeight};
+        unityInstanceRef.SendMessage('XchangeWebGL', 'XchangeWebGLCallBack', JSON.stringify(backData));
+    },
+
+    //创建按钮
+    CreateVirtualBtn: function (data, width, height, x, y) {
+        // alert("CreateVirtualBtn");
+        var info = JSON.parse(data);
+        var id = info["id"];
+        var msg = info["msg"];
+        var eventName = "";
+        var button = document.createElement("button");
+        button.innerHTML = "Virtual Button";
+        button.id = id;
+        button.style.width = width + "px";
+        button.style.height = height + "px";
+        button.style.position = "absolute";
+        button.style.left = x + "px";
+        button.style.top = y + "px";
+        button.style.opacity =0;
+
+        // alert("宽:"+button.style.width + "  高:"+button.style.height + " X:"+button.style.left + "  Y"+ button.style.top+"画布大小" + window.innerWidth  +":"+ window.innerHeight);
+
+        if (id == 1) {
+            eventName = "CopyToClipboard";
+        }
+        button.onclick = function () {
+            // alert("Button Click!");
+            if (id == 1) {
+                XchangeWebGL.CopyToClipboard(msg);
+            }
+            // alert(id);
+        };
+        // 将按钮添加到页面中
+        document.body.appendChild(button);
+
+        var backData = {MethodName: "CreateVirtualBtn", Code: "1", Data: eventName};
+        unityInstanceRef.SendMessage('XchangeWebGL', 'XchangeWebGLCallBack', JSON.stringify(backData));
+        // alert("CreateVirtualBtn End");
+    },
+    //销毁按钮
+    DestroyButton: function (id) {
+        var button = document.getElementById(id);
+        if (button) {
+            document.body.removeChild(button);
+        }
+        var backData = {MethodName: "DestroyButton", Code: "1", Data: button == null};
+        unityInstanceRef.SendMessage('XchangeWebGL', 'XchangeWebGLCallBack', JSON.stringify(backData));
+    },
+
+    CopyToClipboard: function (text) {
+        navigator.clipboard.writeText(text).then(function () {
+            var backData = {MethodName: "CopyToClipboard", Code: "1", Data: "Seccess"};
+            unityInstanceRef.SendMessage('XchangeWebGL', 'XchangeWebGLCallBack', JSON.stringify(backData));
+        }).catch(function (error) {
+            console.error('Failed to copy text: ', error);
+            var backData = {MethodName: "CopyToClipboard", Code: "0", Data: error};
+            unityInstanceRef.SendMessage('XchangeWebGL', 'XchangeWebGLCallBack', JSON.stringify(backData));
+        });
+    },
+};
+
+
+(function () {
+    let input = document.createElement("input");
+    input.type = "text";
+    input.id = "unityInputField";
+    input.style = "position:absolute; top:0; left:0; visibility: hidden; z-index:1000;";
+    input.oninput = window.XchangeWebGL.SendInputToUnity;
+    document.body.appendChild(input);
+})();
